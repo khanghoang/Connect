b73590660bbe9076f3bb8d53769bcdb1b0aaf327
Khang Hoang <hoangtrieukhang@gmail.com>
2015-06-20 00:25:59 +0700

    [UPDATE] production variable

--- app/config/production.config.js
+++ app/config/production.config.js
@@ -2,6 +2,9 @@
 
 module.exports = function (ROOT_PATH) {
   var config = {
+    AWS_KEY: "AKIAIE6SUIZCTS3DO4PA",
+    AWS_SECRET: "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",
+    expiredTime: false,
     server: {
       port: process.env.PORT || 3001,
       hostname: process.env.HOSTNAME || '127.0.0.1',
@@ -16,22 +19,23 @@ module.exports = function (ROOT_PATH) {
     },
     twitterAuth: true,
     twitter: {
-      consumerKey: process.env.TWITTER_KEY,
-      consumerSecret: process.env.TWITTER_SECRET,
+      consumerKey: process.env.TWITTER_KEY || 'HjuVAFjJksaCxec6fZc1jw',
+      consumerSecret: process.env.TWITTER_SECRET  || 'VCMzGTZowbxIVMI5dcqpxwVpzcp2n30eee2DEEGsi7M',
       callbackURL: '/auth/twitter/callback',
       passReqToCallback: true
     },
     facebookAuth: true,
     facebook: {
-      clientID: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
+      clientID: process.env.FACEBOOK_ID || '533758460065977',
+      clientSecret: process.env.FACEBOOK_SECRET || '42fb6d9a8607cc549d5ca194c7422037',
       callbackURL: '/auth/facebook/callback',
       passReqToCallback: true
     },
     mailgun: {
-      user: process.env.MAILGUN_USER,
-      password: process.env.MAILGUN_PASSWORD
+      user: process.env.MAILGUN_USER || 'postmaster@sandbox49936.mailgun.org',
+      password: process.env.MAILGUN_PASSWORD || '1fq8qzwl14w8'
     },
+
     phamtom : {
       retries: 3,
       width       : 1280,
@@ -41,3 +45,4 @@ module.exports = function (ROOT_PATH) {
   }
   return config;
 }
+
