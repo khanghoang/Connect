0ef2010b395084236c0736f1ce56de9c94e9d4ea
Khang Hoang <hoangtrieukhang@gmail.com>
2015-06-29 01:01:51 +0700

    [ADD] success connect by socket.io

--- index.js
+++ index.js
@@ -41,4 +41,9 @@ server.listen(app.get('port'), function() {
   console.log("\n✔ Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
 });
 
-
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
