b08059c9996c18059f28ba1a673f3c54e75e0f63
Khang Hoang <hoangtrieukhang@gmail.com>
2015-07-04 17:12:22 +0700

    [ADD] function to create chat

--- app/controllers/conversation.js
+++ app/controllers/conversation.js
@@ -1,21 +1,42 @@
 'user strict';
+var User = require('../models/user');
+var Conversation = require('../models/conversation');
+var utils = require('../helper/utils');
 
-var Conversation = {
+var ConversationController = {
   createConversationToUserWithToken: function (req, res, next) {
     // start conversation with user
     if (req.user) {
-      return this.createConversationToUserAsUser(req, res, next);
+      return ConversationController.createConversationToUserAsUser(req, res, next);
+    } else {
+      return ConversationController.createConversationToUserAsAnonymous(req, res, next);
     }
-
-    return this.createConversationToUserAsAnnonymous(req, res, next);
   },
 
   createConversationToUserAsAnonymous: function (req, res, next) {
-
+    return utils.responses(res, 200, {message: "Chat with anomymous"});
   },
   createConversationToUserAsUser: function (req, res, next) {
+
+    var targetUserID = req.body.target_user_id;
+    User.find(targetUserID)
+    .exec(function(err, targetUser) {
+
+      if(err) {
+        return utils.responses(res, 400, {message: "Missing or target user id is not valid"});
+      }
+
+      var conversation = new Conversation({});
+      conversation.currentUser = req.user;
+      conversation.targetUser = targetUser;
+
+      conversation.save(function(err, result) {
+        return utils.responses(res, 200, result);
+      })
+
+    })
 
   }
 };
 
-module.exports = Conversation;
+module.exports = ConversationController;
