9b5f92ae59e306df821d26b7854ba05bdb4f8954
Khang Hoang <hoangtrieukhang@gmail.com>
2015-07-04 15:05:36 +0700

    [ADD] conversation model

---
+++ app/models/conversation.js
@@ -0,0 +1,25 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var CreateUpdatedAt = require('mongoose-timestamp');
+var User = require('./user');
+
+var Conversation = new Schema({
+
+  createUser: {
+    type : Schema.ObjectId,
+    ref : 'User'
+  },
+
+});
+
+Conversation.plugin(CreateUpdatedAt);
+
+/**
+ * Methods
+ */
+Conversation.statics.createConversationToUserWithToken = function (token, userID, cb) {
+}
+
+module.exports = mongoose.model('Conversation', Conversation);
+
