86aa57e3044075e4bf18be353f0e0de3f85347ac
Khang Hoang <hoangtrieukhang@gmail.com>
2015-07-04 14:27:39 +0700

    [FIX] packages

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ server.listen(app.get('port'), function() {
 });
 
 io.on('connection', function (socket) {
+  console.log("New connection" + socket);
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
